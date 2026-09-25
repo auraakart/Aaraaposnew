84a16338e18bed7902179f7d1b0ce24eb3d70daf
auraakart <auraakart@gmail.com>
2026-09-25 13:45:55 +0530

    fix(v1.3): remove redundant sqflite test import

--- apps/pos_mobile/test/customer_credit_database_test.dart
+++ apps/pos_mobile/test/customer_credit_database_test.dart
@@ -1,7 +1,6 @@
 import 'package:aaraapos_pos/sell/local_pos_database.dart';
 import 'package:aaraapos_pos/sell/sale_domain.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
